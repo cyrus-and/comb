4cd83c5a486b4dea7b3477e853782fff0904c48a
Andrea Cardaci <cyrus.and@gmail.com>
2018-08-10 20:12:58 +0200

    Fix brief description

--- comb.el
+++ comb.el
@@ -10,10 +10,10 @@
 
 ;;; Commentary:
 
-;; Comb is a native Emacs Lisp solution to search, explore and annotate
+;; Comb is a native Emacs Lisp solution to search, browse and annotate
 ;; occurrences of regular expressions in files.  The interactive interface
 ;; allows to perform an exhaustive classification of all the results to rule out
-;; false positives and asses proper matches.
+;; false positives and asses proper matches during manual static analysis.
 
 ;;; Code:
 
