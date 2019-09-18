a68fd0274592e8dcca28a337c9ee913cb84efa9b
Andrea Cardaci <cyrus.and@gmail.com>
2019-09-18 02:14:43 +0200

    Update copyright year

--- comb-common.el
+++ comb-common.el
@@ -1,6 +1,6 @@
 ;;; comb-common.el --- Common utilities -*- lexical-binding: t -*-
 
-;; Copyright (c) 2018 Andrea Cardaci <cyrus.and@gmail.com>
+;; Copyright (c) 2019 Andrea Cardaci <cyrus.and@gmail.com>
 ;;
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
--- comb-configure.el
+++ comb-configure.el
@@ -1,6 +1,6 @@
 ;;; comb-setup.el --- Configuration buffer -*- lexical-binding: t -*-
 
-;; Copyright (c) 2018 Andrea Cardaci <cyrus.and@gmail.com>
+;; Copyright (c) 2019 Andrea Cardaci <cyrus.and@gmail.com>
 ;;
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
